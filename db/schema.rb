23ab5d170cec27797a38de070431c6118bd94635
Linh Duong <back>
2016-04-05 05:52:46 +0700

    TryEdit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403060743) do
+ActiveRecord::Schema.define(version: 20160404065006) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
