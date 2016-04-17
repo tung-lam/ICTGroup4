2249c1695e92e7d22c6e049447a769fedbf3fc7f
ngockha96 <ngockha96@gmail.com>
2016-04-18 04:59:36 +0700

    Create controllers and styling

---
+++ demoapp5/app/controllers/game2_controller.rb
@@ -0,0 +1,4 @@
+class Game2Controller < ApplicationController
+	def index
+	end
+end
--- demoapp5/config/routes.rb
+++ demoapp5/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "/game1" => "game1#index"
-
+  get "/game2" => "game2#index"
+  get "/game3" => "game3#index"
  
   get "/pages/*page" => "pages#show"
   get "/pages/about" => "pages#about"
