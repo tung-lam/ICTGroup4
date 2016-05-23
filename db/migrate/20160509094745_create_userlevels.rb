f855fbd5f5cf2feccf60fdab8d477e2b67945182
avengerpb <datlequoc96@gmail.com>
2016-05-23 18:11:49 +0700

    default

--- db/migrate/20160509094745_create_userlevels.rb
+++ db/migrate/20160509094745_create_userlevels.rb
@@ -1,7 +1,7 @@
 class CreateUserlevels < ActiveRecord::Migration
   def change
-  	add_column :users, :game1lv, :integer, default: 1
-  	add_column :users, :game2lv, :integer, default: 1
-  	add_column :users, :game3lv, :integer, default: 1
+  	add_column :users, :game1lv, :integer, :default => 1
+  	add_column :users, :game2lv, :integer, :default => 1
+  	add_column :users, :game3lv, :integer, :default => 1
   end
 end
