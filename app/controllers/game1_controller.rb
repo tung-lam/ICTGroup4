a3b74e039daa9794d7c62a9bee5adc8f3e5ee10b
ngockha96 <ngockha96@gmail.com>
2016-05-23 22:27:18 +0700

    increment level update

--- app/controllers/game1_controller.rb
+++ app/controllers/game1_controller.rb
@@ -7,6 +7,11 @@ class Game1Controller < ApplicationController
 	
 		@game1 = Game1lv.find(params[:level])
 		
+		#@game1l = Game1lv.where(:level => @game1.level).limit(1).pluck(:imagelink)
+		#@game1a = Game1lv.where(:level => @game1.level).limit(1).pluck(:answer)
+		#@game1link = @game1l[0].to_s
+		#@game1answer = @game1a[0].to_s
+		
 		@game1link = @game1.imagelink
 		@game1answer = @game1.answer
 		
@@ -20,7 +25,7 @@ class Game1Controller < ApplicationController
 			@game1.update({level: @game1.level + 1})
 			redirect_to game1_play_path(@game1.level), :flash => { :success => "You are right! Now try the next question" }
 
-		elsif @game1answer_user.downcase != @game1answer && @game1answer_user != nil
+		else @game1answer_user.downcase != @game1answer && @game1answer_user != nil
 			#flash.now[:error] = 'You are wrong! Try again'
 		end
 		
