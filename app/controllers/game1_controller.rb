687919d98a36a05fec75b4d04238040dbbdf8cbd
ngockha96 <ngockha96@gmail.com>
2016-05-23 21:57:48 +0700

    increment level update

--- app/controllers/game1_controller.rb
+++ app/controllers/game1_controller.rb
@@ -5,33 +5,23 @@ class Game1Controller < ApplicationController
 	
 	def play
 	
-		# @usergame1lv = current_user.game1lv
-		# @user = User.find("#{@userid}")
-		#if @usergame1lv == [nil]
-		#	@usergame1lv = 1
-		#end
-		
-		
-		#@game1lv = Game1lv.pluck(:level)
-		#@game1lv = 1
-		
 		@game1 = Game1lv.find(params[:level])
 		
-		@game1l = Game1lv.where(:level => @game1).limit(1).pluck(:imagelink)
-		@game1a = Game1lv.where(:level => @game1).limit(1).pluck(:answer)
-		@game1link = @game1l[0].to_s
-		@game1answer = @game1a[0].to_s
+		@game1link = @game1.imagelink
+		@game1answer = @game1.answer
 		
-		@game1answer_user = params["answer"]
+		@game1au = params["answer"]
+		@game1answer_user = @game1au.to_s
+		
+		@game1.level = params[:level]
 	
-		if @game1answer_user == @game1answer.downcase
+		if @game1answer_user.downcase == @game1answer
 			
-			redirect_to game1_play_path(@game1.level += 1), :flash => { :success => "You are right! Now try the next question" }
-			flash.now[:alert] = 'You are rite!'
-			# @user.game1lv += 1
-			# @user.save
-		elsif @game1answer_user != @game1answer.downcase && @game1answer_user != nil
-			flash.now[:error] = 'You are wrong! Try again'
+			@game1.update({level: @game1.level + 1})
+			redirect_to game1_play_path(@game1.level), :flash => { :success => "You are right! Now try the next question" }
+
+		elsif @game1answer_user.downcase != @game1answer && @game1answer_user != nil
+			#flash.now[:error] = 'You are wrong! Try again'
 		end
 		
 	end
