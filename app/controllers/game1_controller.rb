322a36db6f6204b8bf1fee58eb891a5d13b9344a
ngockha96 <ngockha96@gmail.com>
2016-05-23 08:42:53 +0700

    Increment level update

--- app/controllers/game1_controller.rb
+++ app/controllers/game1_controller.rb
@@ -4,31 +4,40 @@ class Game1Controller < ApplicationController
 	end
 	
 	def play
-		@userid = current_user.id
-		@usergame1lv = User.where(id: @userid).limit(1).pluck(:game1lv)	
-		if @usergame1lv == [nil]
-			@usergame1lv = 1
-		end
 	
-		@game1l = Game1lv.where(:level => @usergame1lv).limit(1).pluck(:imagelink)
-		@game1a = Game1lv.where(:level => @usergame1lv).limit(1).pluck(:answer)
+		#@userid = current_user.id
+		#@usergame1lv = User.where(id: @userid).limit(1).pluck(:game1lv)	
+		#if @usergame1lv == [nil]
+		#	@usergame1lv = 1
+		#end
+		
+		
+		#@game1lv = Game1lv.pluck(:level)
+		#@game1lv = 1
+		
+		@game1 = Game1lv.find(params[:level])
+	
+		@game1l = Game1lv.where(:level => @game1).limit(1).pluck(:imagelink)
+		@game1a = Game1lv.where(:level => @game1).limit(1).pluck(:answer)
 		@game1link = @game1l[0].to_s
 		@game1answer = @game1a[0].to_s
+		
+		@game1lv = 1
+		@game1.level = @game1lv
 
 		@game1answer_user = params["answer"]
 		
 		if @game1answer_user == @game1answer
-			redirect_to game1_play_path, :flash => { :success => "You are right!" }
-		else
-			#flash.now[:alert] = 'You are wrong! Lets try again!'
-		end
-	
+			
+			#redirect_to game1_play_path(@game1_next), :flash => { :success => "You are right!" }
 		
-	#giống view cũ	
-	#show @game1link
-	#show @game1answer
-	#if right reload
-	#if not next
+			flash.now[:alert] = 'You are rite!'
+			
+			redirect_to game1_play_path(@game1lv += 1)
+		end
+		
+		
+	
 	end
 	
 	def instruction
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   
   devise_for :users
 
+  resources :game1, only: [:index]
+  
   get "/game1" => "game1#index"
-  get "/game1/play" => "game1#play"
+  get "/game1/play/:level" => "game1#play", as: 'game1_play'
   get "/game1/instruction" => "game1#instruction"
 
  
