049a0e44e777f39d338578b6ec9830fad2ca9e6a
tung-lam <ngstnglm@gmail.com>
2016-05-30 21:29:34 +0700

    add submit javascript function, edit hint

--- app/controllers/game1_controller.rb
+++ app/controllers/game1_controller.rb
@@ -4,7 +4,6 @@ class Game1Controller < ApplicationController
 	end
 	
 	def play
-	
 		@game1 = Game1lv.find(params[:level])
 		
 		#@game1l = Game1lv.where(:level => @game1.level).limit(1).pluck(:imagelink)
@@ -14,26 +13,29 @@ class Game1Controller < ApplicationController
 		
 		@game1link = @game1.imagelink
 		@game1answer = @game1.answer
+
+		@hint = @game1answer[0, @game1answer.length / 2]
 		
 		@game1au = params["answer"]
-		@game1answer_user = @game1au.to_s
+		@game1answer_user = @game1au.to_s.downcase
 		
 		@game1.level = params[:level]
 		
 		@user = User.find(current_user)
-		#@user.lvgame1 = 2
-		#@user.save!
+		@x = 2
 	
-		if @game1answer_user.downcase == @game1answer
-			
+		if (@game1answer_user == @game1answer)
+			@x = 1
+			# flash.now = 'Correct'
 			# @game1.update({level: @game1.level + 1})
 			@user.update({lvgame1: @user.lvgame1 + 1})
-			redirect_to game1_play_path(@game1.level + 1), :flash => { :success => "You are right! Now try the next question" }
-
 			@user.update({rubynumber: @user.rubynumber + 10})
-			
-		elsif @game1answer_user.downcase != @game1answer && @game1answer_user != nil
-			#flash.now[:error] = 'You are wrong! Try again'
+
+			redirect_to game1_play_path(@game1.level + 1)
+		else
+			@x = 0
+		# elsif @game1answer_user != nil
+		# 	flash.now[:error] = 'You are wrong! Try again'
 		end
 	end
 	
